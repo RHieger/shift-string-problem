ae1e5066613a55b69dbeeec491c739d01abde2c5
Robert Hieger <rhieger@gmail.com>
2020-04-04 10:30:34 -0400

    Added in missing package.json “start” script.

---
+++ shift-string.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * shiftString function
+ * 
+ * Solution to Problem Proposed by Landon Schropp
+ * 
+ * Robert Hieger
+ *  
+*/
+
+/**
+ * 
+ * PROBLEM AS STATED BY LANDON SCHROPP:
+ * 
+ * Write a function that shifts each letter in a string forward one character.
+ * For example, the string "abc" should be shifted to "bcd" and the string
+ * "bobby" should be shifted to "cpccz".
+ * 
+ */
+
+ // MY SOLUTION:
+
+ // NOTE: My intention is to use modern JavaScript (latest standard) with Node.js
+ // to allow standalone script execution. It is therefore necessary to have at
+ // least the current LTS distribution of Node.js installed in order to run
+ // this script as intended.
+
+ // URL for Node.js: https://nodejs.org/en/
+
+ let shiftString = (string) => {
+
+    
+
+ };
